c4463966e9ab7f91ff9fdfa4499358ecbe27ba78
Xenoire <xeonexenoire@gmail.com>
2018-11-22 13:07:31 +0700

    Update RegisterActivity.java

--- app/src/main/java/com/xenoire/canvasstream/RegisterActivity.java
+++ app/src/main/java/com/xenoire/canvasstream/RegisterActivity.java
@@ -4,9 +4,11 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -38,10 +40,13 @@ public class RegisterActivity extends AppCompatActivity {
         passwordField = findViewById(R.id.editPassword);
 
         registerButton = findViewById(R.id.buttonRegister);
-
+        Toast.makeText(RegisterActivity.this, "Clicked",
+                Toast.LENGTH_SHORT).show();
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Log.i("CLICKED", "CLICKEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEDDDDD");
                 startRegister();
             }
         });
@@ -49,21 +54,21 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void startRegister() {
-
-        String name = nameField.toString();
-        String email = emailField.toString();
-        String password = passwordField.toString();
-
+        Toast.makeText(RegisterActivity.this, "Start Register",
+                Toast.LENGTH_LONG).show();
+        String name = nameField.getText().toString();
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+        Log.i("CLICKED", password);
         if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
 
             mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
-
                         String user_id = mAuth.getCurrentUser().getUid();
                         DatabaseReference current_user_db = mDatabase.child(user_id);
-                        current_user_db.child("name").setValue(nameField.toString());
+                        current_user_db.child("name").setValue(nameField.getText());
                         current_user_db.child("board");
 
                     }
